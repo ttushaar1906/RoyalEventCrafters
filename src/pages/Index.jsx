e899227f9180b26f8f04d952d46cd80c10abe78e
Tushar Tharwani <ttushaar45@gmail.com>
2023-10-21 11:18:00 +0530

    login

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -5,14 +5,13 @@ import { faFacebook } from '@fortawesome/free-brands-svg-icons';
 import { Link, useNavigate } from "react-router-dom";
 import '../styles/styling.css';
 
-
 export default function Login() {
   const [formData, setFormData] = useState({
     email: "",
     password: "",
   });
 
-const navigate = useNavigate;
+  const navigate = useNavigate(); // You were missing parentheses here.
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
@@ -26,7 +25,7 @@ const navigate = useNavigate;
     e.preventDefault();
     // Send a POST request to your backend with formData
     try {
-      const response = await fetch("/Index", {
+      const response = await fetch("/signup", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
@@ -37,10 +36,10 @@ const navigate = useNavigate;
       // Handle response and redirect if necessary
       if (response.status === 200) {
         // Redirect the user to the dashboard or a success page
-        navigate('/Home')
+        navigate('/Home');
       } else {
         // Handle errors, e.g., display an error message to the user
-        
+        alert("Invalid")
       }
     } catch (error) {
       console.error("Error:", error);
@@ -48,47 +47,45 @@ const navigate = useNavigate;
   };
 
   return (
-    <body className="body">
-      <div>
-        <div className="container form-container">
-          <div className="form-img">
-            <img src="https://i.postimg.cc/26syNgby/Wedding-planner-amico.png" alt="" />
-          </div>
-          <div className="form-cont">
-            <h1 className="lg-heading">Royal Event Crafters</h1>
-            <h2 className="md-heading">Login</h2>
-            <form onSubmit={handleSubmit}>
-              <label htmlFor="email">Enter Email</label>
-              <input
-                type="text"
-                placeholder="Enter Email"
-                name="email"
-                required
-                value={formData.email}
-                onChange={handleInputChange}
-              />
-              <label htmlFor="password">Enter Password</label>
-              <input
-                type="password"
-                placeholder="Enter Password"
-                name="password"
-                required
-                value={formData.password}
-                onChange={handleInputChange}
-              />
-              <button type="submit" className="btn">
-                Login
-              </button>
-            </form>
-            <p>Don't have an account yet?<Link to="/signUp"> Signup</Link></p>
-            <p>OR</p>
-            <div className="social-media">
-              <Link to="https://www.google.com/"><FontAwesomeIcon icon={faGoogle} style={{ color: "#ffffff" }} /></Link>
-              <a href="www.google.com"><FontAwesomeIcon icon={faFacebook} style={{ color: "#ffffff" }} /></a>
-            </div>
+    <div className="body">
+      <div className="container form-container">
+        <div className="form-img">
+          <img src="https://i.postimg.cc/26syNgby/Wedding-planner-amico.png" alt="" />
+        </div>
+        <div className="form-cont">
+          <h1 className="lg-heading">Royal Event Crafters</h1>
+          <h2 className="md-heading">Login</h2>
+          <form onSubmit={handleSubmit}>
+            <label htmlFor="email">Enter Email</label>
+            <input
+              type="text"
+              placeholder="Enter Email"
+              name="email"
+              required
+              value={formData.email}
+              onChange={handleInputChange}
+            />
+            <label htmlFor="password">Enter Password</label>
+            <input
+              type="password"
+              placeholder="Enter Password"
+              name="password"
+              required
+              value={formData.password}
+              onChange={handleInputChange}
+            />
+            <button type="submit" className="btn">
+              Login
+            </button>
+          </form>
+          <p>Don't have an account yet?<Link to="/signUp"> Signup</Link></p>
+          <p>OR</p>
+          <div className="social-media">
+            <a href="https://www.google.com/"><FontAwesomeIcon icon={faGoogle} style={{ color: "#ffffff" }} /></a>
+            <a href="https://www.facebook.com/"><FontAwesomeIcon icon={faFacebook} style={{ color: "#ffffff" }} /></a>
           </div>
         </div>
       </div>
-    </body>
+    </div>
   );
 }
